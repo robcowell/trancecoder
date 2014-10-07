8d6cae4b45ed450f09291845e16d92ddfe15771e
Rob Cowell <robcowell@alphatron.co.uk>
2014-10-07 09:04:59 +0100

    Sorta works in a way

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,9 +3,17 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import java.io.File;
 import java.util.*;
 
+import com.xuggle.mediatool.IMediaReader;
+import com.xuggle.mediatool.IMediaWriter;
+import com.xuggle.mediatool.ToolFactory;
+import com.xuggle.xuggler.IContainer;
+
 import models.*;
+
+
 
 public class Application extends Controller {
 
@@ -13,4 +21,15 @@ public class Application extends Controller {
         render();
     }
 
+    public static void uploadVideo(File video, String outputtype)
+    {
+	String outputFilename = "public/output." + outputtype;
+	IMediaReader reader = ToolFactory.makeReader(video.getPath());
+	IMediaWriter writer = ToolFactory.makeWriter(outputFilename, reader);
+	
+	reader.addListener(writer);
+	while (reader.readPacket() == null);
+	renderArgs.put("file", "/" + outputFilename);
+	render();
+    }
 }
